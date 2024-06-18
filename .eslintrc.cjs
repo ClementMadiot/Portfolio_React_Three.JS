1fe0f5bbc21e9efd2945194f483fdf277d8e3cd1
ClementMadiot <clementmadiot09@gmail.com>
2024-06-18 09:08:13 +0200

    motion annimation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,13 +7,21 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
+    "dependencies": {
+        "next": "12",
+        "react": "17.0.2",
+        "react-dom": "17.0.2"
+      },
+      "devDependencies": {
+        "eslint": "^8.23.0",
+        "eslint-config-next": "^12.2.5"
+      },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
     "react/jsx-uses-react": "error",  
-    "react/no-unknown-property": ["error", { ignore: ["jsx"] }], 
     "react/jsx-uses-vars": "error",
     "react/prop-types": "off",
     "react/jsx-no-target-blank": "off",
@@ -21,6 +29,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    "react/no-unknown-property": ["error", { ignore: ["jsx"] }], 
     
   },
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,6 +20,24 @@ const Hero = () => {
       </div>
 
           <ComputersCanvas/>
+
+          <div className="absolute xs:-bottom-16 bottom-[140px] w-full flex justify-center items-center">
+            <a href="#about">
+              <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
+                <motion.dev
+                animate={{
+                  y:[0, 24, 0]
+                }}
+                transition={{
+                  duration:1.6,
+                  repeat:Infinity,
+                  repeateType: 'loop'
+                }}
+                className="w-3 h-3 p-[7px] rounded-full bg-secondary mb-1"
+                />
+              </div>
+            </a>
+          </div>
     </section>
   );
 };
