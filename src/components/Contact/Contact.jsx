6668ccd1be33d6495537f8f50b46dfa8e2c0c8aa
ClementMadiot <clementmadiot09@gmail.com>
2024-07-21 14:29:48 +0200

    update contact mobile

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -66,7 +66,7 @@ const Contact = () => {
     <Section
       id="contact"
     >
-      <div className="xl:mt-12 xl:flex-row flex gap-10 overflow-hidden">
+      <div className="xl:mt-12 xl:flex-row flex justify-center gap-10 overflow-hidden">
         <motion.div
           variants={slideIn("left", "tween", 0.2, 1)}
           className="flex-[0.75] bg-black-100 p-8 rounded-2xl xl:min-w-[450px]"
