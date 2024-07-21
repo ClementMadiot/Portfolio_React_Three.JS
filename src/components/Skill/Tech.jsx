2e6f6725f18a637e18e10d532b07abafec107155
ClementMadiot <clementmadiot09@gmail.com>
2024-07-21 14:25:03 +0200

    hide Tech to mobile + tablet

--- src/components/Skill/Tech.jsx
+++ src/components/Skill/Tech.jsx
@@ -1,20 +1,27 @@
 import { technologies } from "../../constants";
-import  BallCanvas  from "./Ball";
+import BallCanvas from "./Ball";
 import Section from "../layout/Section";
 import Subheading from "../layout/Subheading";
 
 const Tech = () => {
   return (
-    <>
-      <Subheading title='My Skill' desc='What I know' className='mt-4' />
-    <Section id='skill' className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((tech, index) => (
-        <div id={tech.name} key={`technologie-${index}`} className="w-28 h-28">
-          <BallCanvas name={tech.name} icon={tech.icon} />
-        </div>
-      ))}
-    </Section>
-    </>
+    <div className="hidden lg:block">
+      <Subheading title="My Skill" desc="What I know" className="mt-4" />
+      <Section
+        id="skill"
+        className="flex flex-row flex-wrap justify-center gap-10"
+      >
+        {technologies.map((tech, index) => (
+          <div
+            id={tech.name}
+            key={`technologie-${index}`}
+            className="w-28 h-28"G
+          >
+            <BallCanvas name={tech.name} icon={tech.icon} />
+          </div>
+        ))}
+      </Section>
+    </div>
   );
 };
 
